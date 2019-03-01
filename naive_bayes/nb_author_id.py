832ee849b0d6da27928d8cbf0b0188fa208a6ad3
C Miller <caseysgit@gmail.com>
2019-02-28 19:30:07 -0500

    Fix another typo

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -30,7 +30,7 @@ from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 clf.fit(features_train, labels_train)
 pred = clf.predict(features_test)
-accuracy = clf.score(features_train, labl)
+accuracy = clf.score(features_train, label)
 
 print "Accuracy: {}".format(accuracy)
 
