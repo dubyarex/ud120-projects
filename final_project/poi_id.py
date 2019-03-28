e6c77ef9e023d6ad7e162223db9e58b5b97badf8
C Miller <caseysgit@gmail.com>
2019-03-28 18:35:04 -0400

    Uncomment dump_classifier_and_data() function call

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -115,4 +115,4 @@ features_train, features_test, labels_train, labels_test = \
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
-# dump_classifier_and_data(clf, my_dataset, features_list)
+dump_classifier_and_data(clf, my_dataset, features_list)
